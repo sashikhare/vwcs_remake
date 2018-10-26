33a719d166383e59677bf88c55fbdf7317e8e07e
Sagar Shikhare <sagar.s@peopleinteractive.in>
2018-10-26 11:59:59 +0530

    Formating added

--- src/components/aboutUsPage/AboutusPageBodyDetails_Second.js
+++ src/components/aboutUsPage/AboutusPageBodyDetails_Second.js
@@ -1,152 +1,126 @@
-import React, { Component } from 'react';
-
-
-
-
-import "../../css/linearicons.css"
-import "../../css/font-awesome.min.css"
+import React, { Component } from "react";
+import "../../css/linearicons.css";
+import "../../css/font-awesome.min.css";
 import "../../css/bootstrap.css";
 import "../../css/magnific-popup.css";
-import "../../css/nice-select.css"
+import "../../css/nice-select.css";
 import "../../css/animate.min.css";
-import "../../css/owl.carousel.css"
-import "../../css/jquery-ui.css"
-import "../../css/main.css"
+import "../../css/owl.carousel.css";
+import "../../css/jquery-ui.css";
+import "../../css/main.css";
 class AboutusPageBodyDetails_Second extends React.Component {
-    render() {
-        return (
-            <section class="popular-courses-area section-gap courses-page">
-                <div class="container">
-                    <div class="row d-flex justify-content-center">
-                        <div class="menu-content pb-80 col-lg-11">
-                            <div class="title text-center">
+  render() {
+    return (
+      <section class="popular-courses-area section-gap courses-page">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-80 col-lg-11">
+              <div class="title text-center">
+                <h1 class="mb-10">Our Directors</h1>
 
-                                <h1 class="mb-10">Our Directors</h1>
-
-                                <div class="directorimg header-gap">
-                                    <p>
-                                        <img src="img/d1.jpg" class="dirimg" />
-                                    </p>
-                                </div>
-                                <div class="directortext">
-                                    <p>
-                                        <h4 class="director-name">Mr. R. D. Vyas </h4>
-
-                                        <h5>(AMIE-Mechanical Engg) </h5>
-
-                                        <h5>Chief Executive Officer </h5>
-                                    </p>
-
-                                </div>
-
-                                <div class="directorimg header-gap">
-                                    <p>
-                                        <img src="img/d2.jpg" class="dirimg" />
-                                    </p>
-                                </div>
-                                <div class="directortext">
-                                    <p>
-                                        <h4 class="director-name">Mr. D. K. Dhar </h4>
-
-                                        <h5>(B.Tech-Petroleum Engg) </h5>
-
-                                        <h5>Managing Director</h5>
-                                    </p>
-
-                                </div>
-                                <div class="clear-fix">
-
-                                </div>
-
-                                <div class="directorimg header-gap">
-                                    <p>
-                                        <img src="img/d3.jpg" class="dirimg" />
-                                    </p>
-                                </div>
-                                <div class="directortext">
-                                    <p>
-                                        <h4 class="director-name">Mr. Nishant Vyas</h4>
-
-                                        <h5>(Master in Finance, MBA) </h5>
-
-                                        <h5>Director Finance & Audit</h5>
-                                    </p>
-
-                                </div>
-
-                                <div class="directorimg header-gap">
-                                    <p>
-                                        <img src="img/d4.jpg" class="dirimg" />
-                                    </p>
-                                </div>
-                                <div class="directortext">
-                                    <p>
-                                        <h4 class="director-name">Mr. Ashutosh Dhar</h4>
-
-                                        <h5>(B.Tech-IT, MBA)</h5>
-
-                                        <h5>Director Information & Technology</h5>
-                                    </p>
-
-                                </div>
-
-
-                            </div>
-                            <div class="clear-fix">
-
-                            </div>
-                        </div>
-                    </div>
+                <div class="directorimg header-gap">
+                  <p>
+                    <img src="img/d1.jpg" class="dirimg" />
+                  </p>
                 </div>
-                <div class="container">
-                    <div class="row d-flex justify-content-center">
-                        <div class="menu-content pb-80 col-lg-11">
-                            <div class="title text-center">
-
-                                <h1 class="mb-10">Our Certificates</h1>
-
-                                <div class="directorimg header-gap">
-                                    <p>
-                                        <img src="img/cert1.jpg" class="cirimg" />
-                                    </p>
-                                </div>
-                                <div class="directortext">
-                                    <p>
-                                        <h4 class="director-name">International Association of Drilling Contractors</h4>
-
-                                        <h5>(IDAC: Certificate of Accreditation)</h5>
-
-                                    </p>
-
-                                </div>
-
-                                <div class="directorimg header-gap">
-                                    <p>
-                                        <img src="img/cert2.jpg" class="cirimg" />
-                                    </p>
-                                </div>
-                                <div class="directortext">
-                                    <p>
-                                        <h4 class="director-name">International Well Control Forum</h4>
-
-                                        <h5>(IWCF: Certificate of Membership)</h5>
-
-                        
-                                    </p>
-
-                                </div>
-                            </div>
-                            <div class="clear-fix">
-
-                            </div>
-                        </div>
-                    </div>
+                <div class="directortext">
+                  <p>
+                    <h4 class="director-name">Mr. R. D. Vyas </h4>
+                    <h5>(AMIE-Mechanical Engg) </h5>
+                    <h5>Chief Executive Officer </h5>
+                  </p>
                 </div>
-            </section>
+                <div class="directorimg header-gap">
+                  <p>
+                    <img src="img/d2.jpg" class="dirimg" />
+                  </p>
+                </div>
+                <div class="directortext">
+                  <p>
+                    <h4 class="director-name">Mr. D. K. Dhar </h4>
 
+                    <h5>(B.Tech-Petroleum Engg) </h5>
 
-        );
-    }
+                    <h5>Managing Director</h5>
+                  </p>
+                </div>
+                <div class="clear-fix" />
+
+                <div class="directorimg header-gap">
+                  <p>
+                    <img src="img/d3.jpg" class="dirimg" />
+                  </p>
+                </div>
+                <div class="directortext">
+                  <p>
+                    <h4 class="director-name">Mr. Nishant Vyas</h4>
+
+                    <h5>(Master in Finance, MBA) </h5>
+                    <h5>Director Finance & Audit</h5>
+                  </p>
+                </div>
+
+                <div class="directorimg header-gap">
+                  <p>
+                    <img src="img/d4.jpg" class="dirimg" />
+                  </p>
+                </div>
+                <div class="directortext">
+                  <p>
+                    <h4 class="director-name">Mr. Ashutosh Dhar</h4>
+
+                    <h5>(B.Tech-IT, MBA)</h5>
+
+                    <h5>Director Information & Technology</h5>
+                  </p>
+                </div>
+              </div>
+              <div class="clear-fix" />
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-80 col-lg-11">
+              <div class="title text-center">
+                <h1 class="mb-10">Our Certificates</h1>
+
+                <div class="directorimg header-gap">
+                  <p>
+                    <img src="img/cert1.jpg" class="cirimg" />
+                  </p>
+                </div>
+                <div class="directortext">
+                  <p>
+                    <h4 class="director-name">
+                      International Association of Drilling Contractors
+                    </h4>
+
+                    <h5>(IDAC: Certificate of Accreditation)</h5>
+                  </p>
+                </div>
+                <div class="directorimg header-gap">
+                  <p>
+                    <img src="img/cert2.jpg" class="cirimg" />
+                  </p>
+                </div>
+                <div class="directortext">
+                  <p>
+                    <h4 class="director-name">
+                      International Well Control Forum
+                    </h4>
+
+                    <h5>(IWCF: Certificate of Membership)</h5>
+                  </p>
+                </div>
+              </div>
+              <div class="clear-fix" />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default AboutusPageBodyDetails_Second;
