a061854c2692150f2ee75f2c03a45b1456ce3fd0
Sagar Shikhare <sagar.s@peopleinteractive.in>
2018-10-26 13:16:26 +0530

    Formatting

--- src/contact.js
+++ src/contact.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import "./index.css";
+import './index.css';
 //import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 
-import ContactPageBodyDetails_First from "./components/contatPage/ContactPageBodyDetails_First";
-import ContactPageBodyDetails_Second from "./components/contatPage/ContactPageBodyDetails_Second";
-import ContactPageBodyDetails_Third from "./components/contatPage/ContactPageBodyDetails_Third";
+import ContactPageBodyDetails_First from './components/contatPage/ContactPageBodyDetails_First';
+import ContactPageBodyDetails_Second from './components/contatPage/ContactPageBodyDetails_Second';
+import ContactPageBodyDetails_Third from './components/contatPage/ContactPageBodyDetails_Third';
 
-import FooterDetails from "./components/genric/FooterDetails";
+import FooterDetails from './components/genric/FooterDetails';
 class ContactPage extends React.Component {
   render() {
     return (
